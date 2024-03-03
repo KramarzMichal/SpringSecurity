af88279a5ee8b86b7884b71d485af1013b37a100
Krami <krami2504@gmail.com>
2024-03-03 16:13:21 +0100

    Initial commit

---
+++ src/main/java/com/example/student/Student.java
@@ -0,0 +1,15 @@
+package com.example.student;
+
+public class Student {
+    private final Integer studentId;
+    private String StudentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        StudentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+}
