ec5bd933ffe5ffe072da19dd1fda1d2fe3fa1cf8
Krami <krami2504@gmail.com>
2024-03-09 10:30:58 +0100

    Basic Authentication

--- src/main/java/com/example/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/ApplicationSecurityConfig.java
@@ -1,4 +1,22 @@
 package com.example.security;
 
-public class ApplicationSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() //Wybieramy, że żądanie musi być autoryzowane (czyli posiadać prawo do zasobu)
+                .antMatchers("/", "index.html") //biała list elementów nie wymagających logowania, elementy oddzielamy przcinkami
+                .permitAll()//bez względu na uprawnienia ma dostęp
+                .anyRequest() //określa że dla każdego żądania ma być 5to co ponizej
+                .authenticated() //musi przejść autentykacje (czyli klient musi podać login i hasło)
+                .and()
+                .httpBasic();
+    }
 }
